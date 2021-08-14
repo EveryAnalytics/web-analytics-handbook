0eb4d90078dc8af6ba0edca91ab232ebffd4fb13
mumrik <uoon.dev@gmail.com>
2021-08-14 15:03:24 +0900

    improve(css-in-js): DeviceWidth 타입 추가

--- src/theme/mq.ts
+++ src/theme/mq.ts
@@ -1,7 +1,13 @@
 import facepaint, { DynamicStyleFunction } from 'facepaint';
 
+type DeviceSize = 'xs' | 's' | 'm';
+
+type DeviceWidth = {
+  [key in DeviceSize]: number;
+}
+
 // Todo 지원범위 결정 필요
-const deviceWidth = {
+const deviceWidth: DeviceWidth = {
   xs: 420,
   s: 920,
   m: 1120
