94b5ae8f99738ec78a065e613dfbbcf39225df96
mumrik <uoon.dev@gmail.com>
2021-08-14 21:09:29 +0900

    review: breakpoint 지원 범위 수정

--- src/theme/mq.ts
+++ src/theme/mq.ts
@@ -1,16 +1,13 @@
 import facepaint, { DynamicStyleFunction } from 'facepaint';
 
-type DeviceSize = 'xs' | 's' | 'm';
+type DeviceSize = 'm';
 
 type DeviceWidth = {
   [key in DeviceSize]: number;
 }
 
-// Todo 지원범위 결정 필요
 const deviceWidth: DeviceWidth = {
-  xs: 420,
-  s: 920,
-  m: 1120
+  m: 1000
 }
 
 const mq = (breakpoints = deviceWidth): DynamicStyleFunction => {
