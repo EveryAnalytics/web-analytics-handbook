e1fcff5f129eb0db1f09862c537c0b8821f24005
정대윤 <sensecodevalue>
2021-08-17 00:29:22 +0900

    feat: Add function to get GA_ID in env

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -1,6 +1,15 @@
 import React from "react";
 import Head from "@docusaurus/Head";
 
+const getGAId = () => {
+  const GA_ID = process.env.REACT_APP_GA_ID;
+  if(!GA_ID) {
+    console.warn(`[web-analytics-handbook] Warnning "GA_ID" is not defined. Make sure you created the .env. And Make sure GA_ID is defined in the .env.`);
+  }
+  return GA_ID;
+};
+
+
 const initializeGA = `
   console.info("✅GA가 초기화 되었습니다.");
   window.dataLayer = window.dataLayer || [];
