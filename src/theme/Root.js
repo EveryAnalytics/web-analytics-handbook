90054a10515299a6d742c8f63444b019578f7b29
정대윤 <sensecodevalue>
2021-08-20 01:14:42 +0900

    review: Update variable name GA_ID to GA_TRACKING_ID to match coding convention

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Head from "@docusaurus/Head";
 
-const GA_ID = process.env.REACT_APP_GA_ID;
+const GA_TRACKING_ID = process.env.REACT_APP_GA_TRACKING_ID;
 
-const createInitializeGA = (GA_ID) => {
+const createInitializeGA = (REACT_APP_GA_TRACKING_ID) => {
   
-  if(GA_ID === undefined) {
-    return `console.warn("GA가 초기화 실패되었습니다. "GA_ID"가 정의되지 않았습니다.");`;
+  if(REACT_APP_GA_TRACKING_ID === undefined) {
+    return `console.warn("GA가 초기화 실패되었습니다. "GA_TRACKING_ID"가 정의되지 않았습니다.");`;
   }
   return `
     console.info("✅GA가 초기화 되었습니다.")
@@ -15,20 +15,20 @@ const createInitializeGA = (GA_ID) => {
       dataLayer.push(arguments);
     }
     gtag("js", new Date());
-    gtag("config", "${GA_ID}");
+    gtag("config", "${GA_TRACKING_ID}");
   `
 }
 
-const createGtagPath = (GA_ID) => {
-  if(GA_ID === undefined) {
-    console.warn(`[web-analytics-handbook] Warnning Can't create gtag path. Because "GA_ID" is not defined.`);
+const createGtagPath = (GA_TRACKING_ID) => {
+  if(GA_TRACKING_ID === undefined) {
+    console.warn(`[web-analytics-handbook] Warnning Can't create gtag path. Because "GA_TRACKING_ID" is not defined.`);
     return "";
   }
-  return `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`;
+  return `https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`;
 }
 
-const gtagPath = createGtagPath(GA_ID);
-const initializeGA = createInitializeGA(GA_ID);
+const gtagPath = createGtagPath(GA_TRACKING_ID);
+const initializeGA = createInitializeGA(GA_TRACKING_ID);
 
 function Root({ children }) {
   return (
