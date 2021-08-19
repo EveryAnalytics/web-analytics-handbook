edb5e750e8f086fb88b6a564ca8775a26d22916f
정대윤 <sensecodevalue>
2021-08-20 01:05:35 +0900

    review: Update get GA_ID for simplified functions due to overspack

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -1,20 +1,12 @@
 import React from "react";
 import Head from "@docusaurus/Head";
 
-const getGAId = () => {
-  const GA_ID = process.env.REACT_APP_GA_ID;
-  if(!GA_ID) {
-    console.warn(`[web-analytics-handbook] Warnning "GA_ID" is not defined. Make sure you created the .env. And Make sure GA_ID is defined in the .env.`);
-  }
-  return GA_ID;
-};
-
-const GA_ID = getGAId();
+const GA_ID = process.env.REACT_APP_GA_ID;
 
 const createInitializeGA = (GA_ID) => {
   
   if(GA_ID === undefined) {
-    return `console.warn("GA가 초기화 실패되었습니다.");`;  
+    return `console.warn("GA가 초기화 실패되었습니다. "GA_ID"가 정의되지 않았습니다.");`;
   }
   return `
     console.info("✅GA가 초기화 되었습니다.")
