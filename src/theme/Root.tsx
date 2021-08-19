3f8950ef5c6a51ed5b98791cba4e4e687950cfb3
정대윤 <sensecodevalue>
2021-08-20 02:49:50 +0900

    bug: syntax bug use "" in "" and variable name

--- src/theme/Root.tsx
+++ src/theme/Root.tsx
@@ -3,10 +3,9 @@ import Head from "@docusaurus/Head";
 
 const GA_TRACKING_ID = process.env.REACT_APP_GA_TRACKING_ID;
 
-const createInitializeGA = (REACT_APP_GA_TRACKING_ID) => {
-  
-  if(REACT_APP_GA_TRACKING_ID === undefined) {
-    return `console.warn("GA가 초기화 실패되었습니다. "GA_TRACKING_ID"가 정의되지 않았습니다.");`;
+const createInitializeGA = (GA_TRACKING_ID) => {
+  if(GA_TRACKING_ID === undefined || GA_TRACKING_ID === "") {
+    return `console.warn("GA가 초기화 실패되었습니다. 'GA_TRACKING_ID'가 정의되지 않았습니다.");`;
   }
   return `
     console.info("✅GA가 초기화 되었습니다.")
