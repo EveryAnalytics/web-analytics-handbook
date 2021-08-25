21da797f71937ced33854c4ba06dc7461f2dc033
정대윤 <sensecodevalue@gmail.com>
2021-08-25 16:36:26 +0900

    review: Change Letter를 통한 필터라는 의미에 맞도록 SearchFilter => LetterFilter로 변경

--- src/components/WikiSearch/index.tsx
+++ src/components/WikiSearch/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import SearchFilter from './SearchFilter';
+import SearchFilter from './LetterFilter';
 import SearchInput from './SearchInput';
 
 export default function WikiSearch({
