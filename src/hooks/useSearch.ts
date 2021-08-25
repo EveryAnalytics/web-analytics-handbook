6326d2be5f93a478f302f9faa2daa503fb01086e
정대윤 <sensecodevalue@gmail.com>
2021-08-25 19:09:35 +0900

    review: Refactoring 한글 초성 및 영어 알파벳 정규식 확인, 언어구하기 함수로 변경 및 언어에 따라 맞는 filter하기 switch문으로 변경

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -2,51 +2,66 @@ import { useState } from 'react';
 import { WikiWord } from '../types';
 import KOREAN_CONSONANTS from '../../static/korean-consonants';
 
+const ALPHABET_REGEX = new RegExp(/^[A-Za-z]$/);
+const KOREAN_CONSONANTS_REGEX = new RegExp(/^[ㄱ-ㅎ]$/);
+
+const isLetterFilterValue = (value: string): boolean => {
+  return ALPHABET_REGEX.test(value) || KOREAN_CONSONANTS_REGEX.test(value);
+};
+const getLetterLanguage = (value: string): string => {
+  return KOREAN_CONSONANTS_REGEX.test(value)
+    ? 'KOREAN'
+    : ALPHABET_REGEX.test(value)
+    ? 'ENGLISH'
+    : '';
+};
+
 const useSearch = (source: WikiWord[]) => {
   const [result, setResult] = useState(source);
   const onSearch = (keyword: string) => {
     keyword = keyword.trim();
-    if (
-      keyword.length == 1 &&
-      (/[A-Za-z]/.test(keyword) ||
-        KOREAN_CONSONANTS.findIndex(consonant => consonant === keyword) >= 0)
-    ) {
-      onFilter(keyword);
+    if (isLetterFilterValue(keyword)) {
+      onFilter({ value: keyword, language: getLetterLanguage(keyword) });
     } else {
       setResult(source.filter(item => item.name.includes(keyword)));
     }
   };
-  const onFilter = (value: string) => {
-    value = value.trim();
-    const isKorean = /[ㄱ-ㅎ]/.test(value);
-    const isEnglish = /[A-Za-z]/.test(value);
-    if (value === '' || !value || (!isKorean && !isEnglish)) {
-      setResult(source);
-      return;
-    }
-    if (isKorean) {
-      setResult(
-        source.filter(item => {
-          const code = item.name.trim().charCodeAt(0) - 44032;
-          if (code <= -1 && code >= 11172) {
-            return false;
-          }
-          return KOREAN_CONSONANTS[Math.floor(code / 588)] === value;
-        }),
-      );
-    } else {
-      value = value.toUpperCase();
-      setResult(
-        source.filter(
-          item =>
-            item.name
-              .match(/\((.*?)\)/g)[0]
-              .replace(/[()]/gi, '')
-              .trim()
-              .toUpperCase()
-              .charAt(0) === value,
-        ),
-      );
+
+  const onFilter = ({
+    value,
+    language,
+  }: {
+    value: string;
+    language: string;
+  }) => {
+    switch (language) {
+      case 'KOREAN':
+        setResult(
+          source.filter(item => {
+            const code = item.name.trim().charCodeAt(0) - 44032;
+            if (code <= -1 && code >= 11172) {
+              return false;
+            }
+            return KOREAN_CONSONANTS[Math.floor(code / 588)] === value;
+          }),
+        );
+        break;
+      case 'ENGLISH':
+        value = value.toUpperCase();
+        setResult(
+          source.filter(
+            item =>
+              item.name
+                .match(/\((.*?)\)/g)[0]
+                .replace(/[()]/gi, '')
+                .trim()
+                .toUpperCase()
+                .charAt(0) === value,
+          ),
+        );
+        break;
+      default:
+        setResult([]);
     }
   };
   return {
