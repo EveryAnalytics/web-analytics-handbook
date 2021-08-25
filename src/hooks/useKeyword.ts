91dd009d2ada5dc2a6a1a15c3b72ecbe2847382a
SenseCodeValue <59507527+SenseCodeValue@users.noreply.github.com>
2021-08-25 17:37:32 +0900

    Apply suggestions from code review
    Co-authored-by: Choi Sumin <greatSumini@gmail.com>

--- src/hooks/useKeyword.ts
+++ src/hooks/useKeyword.ts
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-const useKeyword = ({ initKeyword = '' }) => {
+const useKeyword = (initialValue = '') => {
   const [keyword, setKeyword] = useState(initKeyword);
 
   const onChangeKeyword = value => {
