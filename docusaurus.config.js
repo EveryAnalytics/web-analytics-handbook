a66f138d7ff3874f0e31639f6cbbf74c2c98ea0f
JunHee Lee <jhlee910609@gmail.com>
2021-09-05 18:31:19 +0900

    sitemap plugin 추가 (#131)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,6 +1,5 @@
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
-
 const TITLE = 'Web Analytics Handbook';
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
@@ -113,6 +112,11 @@ module.exports = {
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
+        },
+        sitemap: {
+          changefreq: 'weekly',
+          priority: 0.5,
+          trailingSlash: false,
         },
       },
     ],
